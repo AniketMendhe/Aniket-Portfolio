62991fe8c39870eb370e5213dc2ba546ca07d82a
Aniket Shankar Mendhe <162448736+AniketMendhe@users.noreply.github.com>
2024-09-03 13:09:29 +0530

    Update Header.js

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -28,7 +28,7 @@ function Header() {
           <button className="hidden md:inline-block text-white bg-blue-500 px-4 py-2 rounded-full hover:bg-blue-600">
             Linkedin
           </button>
- <button className="border cursor-pointer border-gray-500 px-4 py-2 text-white rounded-full bg-slate-500 hover:bg-slate-800" onClick={()=>router.push("https://github.com/AniketMendhe/Aniket-Portfolio")}>
+ <button className="border cursor-pointer border-gray-500 px-4 py-2 text-white rounded-full bg-slate-500 hover:bg-slate-800" onClick={()=>router.push("https://github.com/AniketMendhe")}>
             Github
           </button>
         </div>
